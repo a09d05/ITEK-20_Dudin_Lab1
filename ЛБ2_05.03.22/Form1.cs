4860359c62f4ffda4d8e80679d50dd2e9b990d3a
a09d05 <d.sasha.2021@gmail.com>
2022-03-14 10:26:55 +0300

    Lab-1

---
+++ ЛБ2_05.03.22/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ЛБ2_05._03._22
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // сложение
+        {
+            int a, b;
+            a = Int32.Parse(textBox1.Text);
+            b = Int32.Parse(textBox2.Text);
+
+            int c = a + b;
+            textBox3.Text = c.ToString();
+            label5.Text = "Сложение";
+        }
+
+        private void button2_Click(object sender, EventArgs e) // вычитание
+        {
+            int a, b;
+            a = Int32.Parse(textBox1.Text);
+            b = Int32.Parse(textBox2.Text);
+
+            int c = a - b;
+            textBox3.Text = c.ToString();
+            label5.Text = "Вычитание";
+        }
+
+        private void button3_Click(object sender, EventArgs e) // умножение 
+        {
+            int a, b;
+            a = Int32.Parse(textBox1.Text);
+            b = Int32.Parse(textBox2.Text);
+
+            int c = a * b;
+            textBox3.Text = c.ToString();
+            label5.Text = "Умножение";
+        }
+
+        private void button4_Click(object sender, EventArgs e) // деление
+        {
+            int a, b;
+            a = Int32.Parse(textBox1.Text);
+            b = Int32.Parse(textBox2.Text);
+
+            if(b != 0)
+            {
+                int c = a / b;
+                textBox3.Text = c.ToString();
+                label5.Text = "Деление";
+            }
+            else 
+            {
+                textBox3.Text = " ";
+                label5.Text = "Делить на 0 нельзя!";
+            }
+        }
+    }
+}
